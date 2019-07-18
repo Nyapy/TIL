edec77ad91b27f2cc9f775d842e4711618b01bab
Nyapy <nyapy@naver.com>
2019-07-18 18:22:24 +0900

    190718

--- practice.py
+++ practice.py
@@ -1,14 +1,26 @@
-'''
-안녕
-수경아
-있잖아
-이건 사실
-문자열이야
-'''
-print('''
-안녕
-수경아
-있잖아
-이건 사실
-문자열이야
-''')
+# def is_palindrom(word):
+#     if word == word[::-1]:
+#         return True
+#     else:
+#         return False
+
+
+
+# print(is_palindrom('level'))
+# print(is_palindrom('가져가라 가져가'))
+
+# def is_palindrom(word):
+#     list_word = list(word)
+#     for i in range(len(list_word) //2):
+#         if list_word[i] != list_word[-i-1]:
+#             return False
+#     return True
+
+# print(is_palindrom('가자미'))
+
+# print(dir(__builtins__))
+
+def ssafy(name, location = '서울') :
+    print(f'{name}의 지역은 {location}입니다')
+
+ssafy('허준' )
