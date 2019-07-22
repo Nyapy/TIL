979b5b91848ad980a5c075a6c88a80afe926a91b
Nyapy <nyapy@naver.com>
2019-07-22 18:05:08 +0900

    190722

--- practice.py
+++ practice.py
@@ -1,26 +1,12 @@
-# def is_palindrom(word):
-#     if word == word[::-1]:
-#         return True
-#     else:
-#         return False
+import datetime
+date = datetime.date(2019,7,13)
+print(f'{date.year}{date.month}{date.day}')
 
+print(date.strftime('%Y%m%d'))
 
+print(date)
 
-# print(is_palindrom('level'))
-# print(is_palindrom('가져가라 가져가'))
-
-# def is_palindrom(word):
-#     list_word = list(word)
-#     for i in range(len(list_word) //2):
-#         if list_word[i] != list_word[-i-1]:
-#             return False
-#     return True
-
-# print(is_palindrom('가자미'))
-
-# print(dir(__builtins__))
-
-def ssafy(name, location = '서울') :
-    print(f'{name}의 지역은 {location}입니다')
-
-ssafy('허준' )
+for i in range(51):
+   t_time = date - datetime.timedelta(weeks=i)
+   targetDt = t_time.strftime('%Y%m%d')
+   print(targetDt)
