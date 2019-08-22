2c12db2c19f7dddd869425905688387ab193e7d5
Nyapy <nyapy@naver.com>
2019-08-22 19:54:26 +0900

    190822

---
+++ 04_algorythm/08day/농작물 수확하기.py
@@ -0,0 +1,26 @@
+import sys
+
+sys.stdin = open('농작물 수확하기.txt')
+
+T = int(input())
+
+for tc in range(T):
+    N = int(input())
+    farm = [list(map(int, input().split())) for _ in range(N)]
+
+    # print(farm)
+    cnt = 0
+    tot = 0
+    N2 = N//2
+    for i in range(N):
+        if i <= N//2:
+            for j in range((N//2)-cnt, (N//2)+cnt+1):
+                tot += farm[i][j]
+                cnt +=1
+        if i > N//2:
+            for j in range((N//2)-cnt, (N//2)+cnt+1):
+                tot += farm[i][j]
+                cnt -=1
+
+
+    print(tot)
