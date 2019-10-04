c1747e038f2f6630ccfa784668a55b932310ec80
Nyapy <nyapy@naver.com>
2019-10-04 17:20:32 +0900

    191004

--- 03_django/04_django_crud_review/articles/views.py
+++ 03_django/04_django_crud_review/articles/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.core.exceptions import ValidationError
 from .models import Article
 
 # Create your views here.
@@ -15,24 +16,54 @@ def new(request):
     return render(request, 'articles/new.html')
 
 def create(request):
-    title = request.POST.get('title')
-    print(title)
-    content = request.POST.get('content')
+    try:
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        article = Article(title=title, content= content)
+        article.full_clean()
+    except ValidationError:
+        raise ValidationError('Your Error Message')
+    else:
+        article.save()
+        return redirect('articles:detail', article.pk)
+    # title = request.POST.get('title')
+    # print(title)
+    # content = request.POST.get('content')
     
-    #1. 첫번째 방법
-    # article = Article()
-    # article.title = title
-    # article.content = content
-    # article.save()
+    # #1. 첫번째 방법
+    # # article = Article()
+    # # article.title = title
+    # # article.content = content
+    # # article.save()
 
-    #2. 두번째 방법
+    # #2. 두번째 방법
     # article = Article(title=title, content = content)
     # article.save()
 
-    #3. 세번째 방법
-    Article.objects.create(title=title, content=content)
+    # #3. 세번째 방법
+    # # Article.objects.create(title=title, content=content)
 
-    return redirect('/articles/')
+
+    # return redirect(f'/articles/{article.pk}')
 
 def detail(request, pk):
-    pass
+    article = Article.objects.get(pk=pk)
+    context = {'article':article}
+    return render(request, 'articles/detail.html', context)
+
+def delete(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.delete()
+    return redirect('articles:index')
+
+def edit(request, pk):
+    article = Article.objects.get(pk=pk)
+    context = {'article':article }
+    return render(request, 'articles/edit.html', context)
+
+def update(request, pk):
+    article = Article.objects.get(pk=pk)
+    article.title = request.POST.get('title')
+    article.content = request.POST.get('content')
+    article.save()
+    return redirect('articles:detail', article.pk)
