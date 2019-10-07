fc180b976c25f22e677154fb63bb01836f4d62ae
Nyapy <nyapy@naver.com>
2019-10-07 17:46:37 +0900

    191007

--- 03_django/04_django_crud_review/articles/urls.py
+++ 03_django/04_django_crud_review/articles/urls.py
@@ -5,11 +5,11 @@ app_name = 'articles'
 
 
 urlpatterns =[
-    path('', views.index, name='index'),
-    path('new/', views.new, name='new'),
-    path('create/', views.create, name='create'),
-    path('<int:pk>/', views.detail, name='detail'),
-    path('<int:pk>/delete/', views.delete, name='delete'),
-    path('<int:pk>/edit/', views.edit, name='edit'),
-    path('<int:pk>/update/', views.update, name='update'),
+    path('', views.index, name='index'), #GET
+    # path('new/', views.new, name='new'),
+    path('create/', views.create, name='create'), #GET(new), POST(create)
+    path('<int:pk>/', views.detail, name='detail'), #GET
+    path('<int:pk>/delete/', views.delete, name='delete'), #POST
+    # path('<int:pk>/edit/', views.edit, name='edit'),
+    path('<int:pk>/update/', views.update, name='update'), # GET(edit), POST(update)
 ]
--- 03_django/04_django_crud_review/articles/views.py
+++ 03_django/04_django_crud_review/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.core.exceptions import ValidationError
 from .models import Article
+from IPython import embed
 
 # Create your views here.
 
@@ -12,20 +13,19 @@ def index(request):
     context = {'articles':articles}
     return render(request, 'articles/index.html', context)
 
-def new(request):
-    return render(request, 'articles/new.html')
 
 def create(request):
-    try:
+    #포스트 요청일 때
+    if request.method == 'POST':
         title = request.POST.get('title')
         content = request.POST.get('content')
         article = Article(title=title, content= content)
-        article.full_clean()
-    except ValidationError:
-        raise ValidationError('Your Error Message')
-    else:
         article.save()
         return redirect('articles:detail', article.pk)
+
+    #겟 요청일 때
+    else:
+        return render(request, 'articles/create.html')
     # title = request.POST.get('title')
     # print(title)
     # content = request.POST.get('content')
@@ -52,18 +52,28 @@ def detail(request, pk):
     return render(request, 'articles/detail.html', context)
 
 def delete(request, pk):
-    article = Article.objects.get(pk=pk)
-    article.delete()
-    return redirect('articles:index')
+    if request.method == 'POST':
+        article = Article.objects.get(pk=pk)
+        article.delete()
+        return redirect('articles:index')
+    else:
+        return redirect('articles:index')
 
-def edit(request, pk):
-    article = Article.objects.get(pk=pk)
-    context = {'article':article }
-    return render(request, 'articles/edit.html', context)
+
+# def edit(request, pk):
+#     article = Article.objects.get(pk=pk)
+#     context = {'article':article }
+#     return render(request, 'articles/edit.html', context)
 
 def update(request, pk):
     article = Article.objects.get(pk=pk)
-    article.title = request.POST.get('title')
-    article.content = request.POST.get('content')
-    article.save()
-    return redirect('articles:detail', article.pk)
+
+    if request.method == 'POST':
+        article.title = request.POST.get('title')
+        article.content = request.POST.get('content')
+        article.save()
+        return redirect('articles:detail', article.pk)
+
+    else:
+        context = {'article':article }
+        return render(request, 'articles/update.html', context)
