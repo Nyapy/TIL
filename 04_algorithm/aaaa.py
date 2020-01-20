392c9b63e518c31f11b1aad197e6b7a425c20982
이현우 <nyapy@naver.com>
2020-01-20 19:11:04 +0900

    200120

--- 04_algorithm/aaaa.py
+++ 04_algorithm/aaaa.py
@@ -7,3 +7,4 @@ for i in range(1,1<<N):
             print(T[j], end=" ")
     print()
 
+
--- 04_algorithm/백준/Study/1022소용돌이예쁘게출력하기.py
+++ 04_algorithm/백준/Study/1022소용돌이예쁘게출력하기.py
@@ -4,32 +4,36 @@ sys.stdin = open("1022.txt")
 
 r1,c1,r2,c2 = map(int, input().split())
 
+num = max(map(abs, [r1,c1,r2,c2]))
+
 dx = [1, 0, -1,0]
 dy = [0, -1, 0,1]
 
-soyong = [[0 for _ in range(10002)] for _ in range(10002)]
+soyong = [[0 for _ in range(num*2+1)] for _ in range(num*2+1)]
 
-x = 5000
-y = 5000
+x = num
+y = num
 cnt = 1
 
 flag = 0
 n = 0
 
+soyong[y][x] = 1
+
 length = 1
-while cnt <= 100000000:
+while cnt <= ((num*2+1)**2):
     for i in range(length):
         nx = x + dx[n]
         ny = y + dy[n]
         cnt += 1
         soyong[ny][nx] = cnt
 
-        if cnt == 100000000:
+        if cnt == ((num*2+1)**2):
             break
 
         x = nx
         y = ny
-    if cnt == 100000000:
+    if cnt == ((num*2+1)**2):
         break
     flag += 1
 
@@ -39,7 +43,7 @@ while cnt <= 100000000:
 
     n = (n + 1) % 4
 
-for i in range(5000-r1,5000+r2):
-    for j in range(5000-c1,5000+c2):
-        print(soyong[i][j], end = " ")
+for i in range(num+r1,num+r2+1):
+    for j in range(num+c1,num+c2+1):
+        print("{}" .format(soyong[i][j]), end = " ")
     print()
