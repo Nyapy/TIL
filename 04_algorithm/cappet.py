d7e175a15eed1ac0d4b1287445698113e4f19770
이현우 <nyapy@naver.com>
2020-06-29 01:44:02 +0900

    ssafy

--- 04_algorithm/cappet.py
+++ 04_algorithm/cappet.py
@@ -1,14 +1,23 @@
-brown = 10
-red = 2
+brown = 24
+yellow = 24
 
-de = (4+brown)**2-(4*2)*(2*red+2*brown)
+def cappet():
+    de = (4+brown)**2-(4*2)*(2*yellow+2*brown)
+    a = de**(0.5)/4
+    aa = (4+brown)/4
+    garo = a+aa
+    sero = aa-a
+    answer = [garo,sero]
+    return list(map(int, answer))
 
-a = de**(0.5)/4
 
-aa = (4+brown)/4
+def cappet2():
+    cap = brown + yellow
+    for i in range(3,cap//3+1):
+        for j in range(i, cap//i+1):
+            if i*j == cap:
+                if (i-2)*(j-2)==yellow:
+                    answer = [j,i]
+                    return answer
 
-garo = a+aa
-sero = aa-a
-ans = [garo,sero]
-
-print(list(map(int, ans)))
+print(cappet(),cappet2())
